b11e3a817e81b1a3a49c8a78f95a5c50718a74f9
Afrijal Dzuhri <hi@afrijal.dev>
2024-12-17 23:38:23 +0700

    feat: add firebase config

---
+++ apps/backend-repo/config/firebaseConfig.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+export const firebaseConfig = {
+  apiKey: "AIzaSyDW-RVkyF4FBM3okNMHfCyjsljDWM7bNVs",
+  authDomain: "ebuddy-test-a785f.firebaseapp.com",
+  projectId: "ebuddy-test-a785f",
+  storageBucket: "ebuddy-test-a785f.firebasestorage.app",
+  messagingSenderId: "336168882105",
+  appId: "1:336168882105:web:db2a7fb649413fd580b068",
+};
+
+export const app = initializeApp(firebaseConfig);
