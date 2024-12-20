f2229b99abea5691897603392ed114195433f358
Afrijal Dzuhri <hi@afrijal.dev>
2024-12-20 22:25:38 +0700

    fix: change base api route

--- apps/backend-repo/core/app.ts
+++ apps/backend-repo/core/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import routes from '../routes'
+import routes from "../routes";
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 
-app.use(routes());
+app.use("/api", routes());
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
